95cb55de30b7c57dcf3a492a35682d33750c70da
佐藤太一 <3101taichi@gmail.com>
2021-07-07 08:15:45 +0900

    環境変数テスト2

--- app/app.py
+++ app/app.py
@@ -2,3 +2,6 @@ import os
 
 print("test_value" == os.environ.get('TEST'))
 print("test" == os.environ.get('TEST'))
+
+print(os.environ.get('TEST2'))
+print("test_value_2" == os.environ.get('TEST2'))
