b7c006638e2f8550297ca1ec5b26108de98f493c
佐藤太一 <3101taichi@gmail.com>
2021-07-07 11:06:02 +0900

    test

--- app/app.py
+++ app/app.py
@@ -2,6 +2,4 @@ import os
 
 print("test_value" == os.environ.get('TEST'))
 print("test" == os.environ.get('TEST'))
-
-print(os.environ.get('TEST2'))
 print("test_value_2" == os.environ.get('TEST2'))
